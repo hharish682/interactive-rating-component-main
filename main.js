ea0f9c9f6d85c1c7e82a84a03451cfddf732ff90
Harish <hharish682@gmail.com>
2023-04-06 14:59:21 +0300

    commit

--- main.js
+++ main.js
@@ -1,59 +1,32 @@
 "use strict";
 
-const n = document.querySelectorAll(".num");
-const num_1 = document.querySelector(".num_1");
-const num_2 = document.querySelector(".num_2");
-const num_3 = document.querySelector(".num_3");
-const num_4 = document.querySelector(".num_4");
-const num_5 = document.querySelector(".num_5");
+let n = document.querySelectorAll(".num");
 let value = document.querySelector(".value");
 const box_one = document.querySelector(".box_one");
 const box_two = document.querySelector(".box_two");
 let btn = document.querySelector(".btn");
 
-const a = [num_1, num_2, num_3, num_4, num_5];
+const a = [1, 2, 3, 4, 5];
 
-for (let i = 0; i < a.length; i++) {
+for (let i = 0; i < n.length; i++) {
+  /* Add Class function */
+
+  function addclass() {
+    n[i].classList.add("active");
+    value.textContent = n[i].textContent;
+  }
+  /* Remove Class function */
+  function removeclass() {
+    n[i].classList.remove("active");
+  }
+
+  /*selection click function */
+
   n[i].addEventListener("click", function () {
-    // n[i].classList.add("active");
+    n[i] = n[i] == a[i] ? addclass() : removeclass();
 
-    if (n[i] === num_1) {
-      value.textContent = 1;
-      num_1.classList.add("active");
-    } else {
-      num_1.classList.remove("active");
-    }
-
-    if (n[i] === num_2) {
-      value.textContent = 2;
-      num_2.classList.add("active");
-    } else {
-      num_2.classList.remove("active");
-    }
-    if (n[i] === num_3) {
-      value.textContent = 3;
-      num_3.classList.add("active");
-    } else {
-      num_3.classList.remove("active");
-    }
-    if (n[i] === num_4) {
-      value.textContent = 4;
-      num_4.classList.add("active");
-    } else {
-      num_4.classList.remove("active");
-    }
-    if (n[i] === num_5) {
-      value.textContent = 5;
-      num_5.classList.add("active");
-    } else {
-      num_5.classList.remove("active");
-    }
+    /*Submit Button */
   });
-}
-
-/*Submit Button */
-
-for (let i = 0; i < a.length; i++) {
   btn.addEventListener("click", function () {
     if (n[i].classList.contains("active")) {
       box_two.classList.add("visible");
