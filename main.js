d0a35155b0f31f3ffeefa422b18be5d8ce068ba8
Harish <hharish682@gmail.com>
2023-04-07 09:33:25 +0300

    commit

--- main.js
+++ main.js
@@ -6,28 +6,20 @@ const box_one = document.querySelector(".box_one");
 const box_two = document.querySelector(".box_two");
 let btn = document.querySelector(".btn");
 
-const a = [1, 2, 3, 4, 5];
+const a = [0, 1, 2, 3, 4];
 
 for (let i = 0; i < n.length; i++) {
-  /* Add Class function */
-
-  function addclass() {
-    n[i].classList.add("active");
-    value.textContent = n[i].textContent;
-  }
-  /* Remove Class function */
-  function removeclass() {
-    n[i].classList.remove("active");
-  }
-
-  /*selection click function */
-
-  n[i].addEventListener("click", function () {
-    n[i] += a[i] ? addclass() : removeclass();
+  n[i].addEventListener("click", () => {
+    if (n[i] === i) {
+      n[i].classList.add("active");
+      value.textContent = n[i].textContent;
+    } else {
+      n[i].classList.remove("active");
+    }
 
     /*Submit Button */
   });
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", () => {
     if (n[i].classList.contains("active")) {
       box_two.classList.add("visible");
       box_one.classList.add("invisible");
